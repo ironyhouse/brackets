938f1938d5c54a0d5bbd550f9a8987924729811a
ironyhouse <ironylait@gmail.com>
2020-03-06 13:19:21 +0300

    feat: finished Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = bracketsConfig.map(i => i.join(''));
+  let lngth = str.length + 2;
+
+  do {
+    for (let i = 0; i < config.length; i++) {
+      str = str.replace(config[i], '');
+    }
+    lngth--;
+  } while (lngth !== str.length);
+  
+  if (str === '') {
+      return true;
+  } else {
+      return false;
+  }
 }
